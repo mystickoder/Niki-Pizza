9b753f15eaabab4f185d1f7ba02b8ec9ccf773a1
mystickoder <89115482+mystickoder@users.noreply.github.com>
2021-09-30 15:14:52 -0400

    Add files via upload
    Niki Pizza

---
+++ Niki-Pizza.py
@@ -0,0 +1,20 @@
+pizza = ['onions','cheese', 'tomatoes', 'mushrooms']
+if "olives" in pizza:
+    print("we add olives")
+    
+else:
+    print("we have no olives")
+
+if "cheese" in pizza:
+    print("add provolone")
+
+if "sausage" in pizza:
+    print("add pepperoni")
+
+else:
+    print("no sausage on menu")
+
+if "mushrooms" in pizza:
+    print("make this one a veggie")
+
+print("Thanks for visiting Niki's Pizza")
